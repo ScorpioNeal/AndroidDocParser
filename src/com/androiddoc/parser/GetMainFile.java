24952187cde2be1bb16f44b25d8998433b74e1b7
ScorpioNeal <budongxu@gmail.com>
2014-03-18 11:33:18 +0800

    get all html under design tag

--- src/com/androiddoc/parser/GetMainFile.java
+++ src/com/androiddoc/parser/GetMainFile.java
@@ -7,21 +7,31 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-
 /**
- * 获取主要内容
- * Design: GetStarted,Style,Pattern,BuildingBlocks,Downloads, Videos
+ * 获取主要内容 Design: GetStarted,Style,Pattern,BuildingBlocks,Downloads, Videos
  * Develop: Training, APIGuides, Tools
+ * 
  * @author ScorpioNeal2013
- *
+ * 
  */
 public class GetMainFile {
 
+	private static final String BASEURL = "http://developer.android.com";
+
 	public static void main(String[] args) {
 		try {
-			Document doc = Jsoup.connect("http://developer.android.com/index.html").get();
-			Elements designElements = doc.select("div>div#quicknav>ul>li.design>ul>li");
-			System.out.println(designElements.size());
+			Document doc = Jsoup.connect(
+					"http://developer.android.com/design/index.html").get();
+			Elements mElements = doc
+					.select("ul#nav>li.nav-section");
+			for(Element mElement : mElements){
+				Element parentElement = mElement.select("div").first();
+				Elements childElements = mElement.select("ul>li");
+				System.out.println(parentElement.text() +" (" + parentElement.child(0).attr("href") + ")");
+				for(Element childElement : childElements){
+					System.out.println("\t" + childElement.text() + "  " + childElement.child(0).attr("href"));
+				}
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
